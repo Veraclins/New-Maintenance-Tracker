9bf7a230fdd70e9aee892ce57c4aac02185360a0
Clinton Agada <clinton.agada@andela.com>
2018-08-16 18:17:28 +0100

    Add more tests and oprimizations

--- src/database/index.js
+++ src/database/index.js
@@ -3,8 +3,8 @@ import { Pool } from 'pg';
 // This instantiates a connection pool that can be imported and used in other places
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false, // for local development
-  // ssl: true, // for production
+  // ssl: false, // for local development
+  ssl: true, // for production
 });
 
 
